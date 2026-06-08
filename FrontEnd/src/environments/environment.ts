558224b804fda95172b2e9644de83b11b942f443
Vishal Kumar~ <vishalbth99@gmail.com>
2026-06-09 02:47:14 +0530

    Added Netlify redirects

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5007/api',
+  apiUrl: 'https://task-management-app-1-qcv8.onrender.com/api',
   jwtTokenKey: 'task-management-jwt'
 };
